1ed117964483870cc1eedc4bff67d899b7b7ec47
The Moose <moose@192.168.138.147>
2023-11-09 15:29:02 +0800

    9Nov2023 - reviews ongoing - fontend

--- components/ui-ux/Reviews.tsx
+++ components/ui-ux/Reviews.tsx
@@ -1,9 +1,8 @@
 import { GlobalState } from "@/global-entities";
-import { calculateReviewRatingAverage } from "@/utils";
-import { StarIcon } from "@heroicons/react/20/solid";
 import { useSelector } from "react-redux";
 import ReviewForm from "../forms/ReviewForm";
 import ReviewList from "../list-view/ReviewList";
+import ReviewRatingWidget from "./restaurants/ReviewRatingWidget";
 
 const reviews = {
   average: 4,
@@ -66,72 +65,7 @@ const Reviews = () => {
             Customer Reviews
           </h2>
 
-          <div className="mt-3 flex items-center">
-            <div>
-              <div className="flex items-center">
-                {[0, 1, 2, 3, 4].map((rating) => (
-                  <StarIcon
-                    key={rating}
-                    className={classNames(
-                      reviews.average > rating
-                        ? "text-yellow-400"
-                        : "text-gray-300",
-                      "h-5 w-5 flex-shrink-0"
-                    )}
-                    aria-hidden="true"
-                  />
-                ))}
-              </div>
-              <p className="sr-only">{reviews.average} out of 5 stars</p>
-            </div>
-            <p className="ml-2 text-sm text-gray-900">
-              Based on {reviews.totalCount} reviews
-            </p>
-          </div>
-
-          <div className="mt-6">
-            <h3 className="sr-only">Review data</h3>
-
-            <dl className="space-y-3">
-              {reviews.counts.map((count) => (
-                <div key={count.rating} className="flex items-center text-sm">
-                  <dt className="flex flex-1 items-center">
-                    <p className="w-3 font-medium text-gray-900">
-                      {count.rating}
-                      <span className="sr-only"> star reviews</span>
-                    </p>
-                    <div
-                      aria-hidden="true"
-                      className="ml-1 flex flex-1 items-center"
-                    >
-                      <StarIcon
-                        className={classNames(
-                          count.count > 0 ? "text-yellow-400" : "text-gray-300",
-                          "h-5 w-5 flex-shrink-0"
-                        )}
-                        aria-hidden="true"
-                      />
-
-                      <div className="relative ml-3 flex-1">
-                        <div className="h-3 rounded-full border border-gray-200 bg-gray-100" />
-                        {count.count > 0 ? (
-                          <div
-                            className="absolute inset-y-0 rounded-full border border-yellow-400 bg-yellow-400"
-                            style={{
-                              width: `calc(${count.count} / ${reviews.totalCount} * 100%)`,
-                            }}
-                          />
-                        ) : null}
-                      </div>
-                    </div>
-                  </dt>
-                  <dd className="ml-3 w-10 text-right text-sm tabular-nums text-gray-900">
-                    {Math.round((count.count / reviews.totalCount) * 100)}%
-                  </dd>
-                </div>
-              ))}
-            </dl>
-          </div>
+          <ReviewRatingWidget />
 
           <div className="mt-10">
             <h3 className="text-lg font-medium text-gray-900">
